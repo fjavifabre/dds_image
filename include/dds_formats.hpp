3dca59edd9af0042d10a9fd6a70b80d3034f8c67
Fco Javier Fabre <fjavifabre@gmail.com>
2026-03-23 18:15:23 +0100

    New release/v0.1.1 (#1)
    * Add additional function to check just for magic number at the start of the stream
    
    * Update reading to use new function and early reject non-dds files
    
    * Format dds.hpp
    
    * Format dds-formats.hpp
    
    * Fix to const issue
    
    * Fix span emplace_back size type mismatch for C++20 std::span compatibility

--- include/dds_formats.hpp
+++ include/dds_formats.hpp
@@ -14,11 +14,11 @@
     static_cast<uint32_t>(char1) | (static_cast<uint32_t>(char2) << 8) | (static_cast<uint32_t>(char3) << 16) |                            \
         (static_cast<uint32_t>(char4) << 24)
 
-#define UNDO_FOUR_CHARACTER_CODE(x, name)                                                                                            \
-    unsigned char name[4];                                                                                                           \
-    name[0] = x >> 0;                                                                                                                \
-    name[1] = x >> 8;                                                                                                                \
-    name[2] = x >> 16;                                                                                                               \
+#define UNDO_FOUR_CHARACTER_CODE(x, name)                                                                                                  \
+    unsigned char name[4];                                                                                                                 \
+    name[0] = x >> 0;                                                                                                                      \
+    name[1] = x >> 8;                                                                                                                      \
+    name[2] = x >> 16;                                                                                                                     \
     name[3] = x >> 24;
 
 #if __cplusplus >= 201703L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201703L)
@@ -321,6 +321,6 @@ namespace dds {
         bool supportsAlpha = false;
         DXGI_FORMAT format;
         std::unique_ptr<uint8_t[]> data = nullptr;
-        std::vector<dds::span<uint8_t>> mipmaps;
+        std::vector<dds::span<const uint8_t>> mipmaps;
     };
 } // namespace dds
